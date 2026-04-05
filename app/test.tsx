6f0cf780b79edb5b97b1939b00e693e8381bd6b6
cs36577 <melyssa1857@gmail.com>
2026-04-05 19:33:43 +0700

    login page

---
+++ app/test.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Test() {
+    return (
+        <div className="h-screen w-screen bg-surface flex items-center justify-center px-4 py-6">
+            TEST page
+        </div>
+    )
+}
---
+++ app/test/page.tsx
@@ -0,0 +1,7 @@
+export default function Test() {
+    return (
+        <div className="h-screen bg-background flex items-center justify-center px-4 py-6">
+            TEST page
+        </div>
+    )
+}
