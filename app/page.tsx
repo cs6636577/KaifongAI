c4649ffc6d11792987030ae43803a7f1abef9a31
cs36577 <melyssa1857@gmail.com>
2026-04-05 18:56:04 +0700

    login page

--- app/page.tsx
+++ app/page.tsx
@@ -28,12 +28,20 @@ export default function Home() {
               </div>
               <form className="w-full max-w-sm mx-auto">
                 <div className="mb-5 relative flex items-center">
-                  <span className="absolute left-4 text-gray-400 text-xl">👤</span>
-                  <input className="w-full pl-12 pr-4 py-3 border-2 border-gray-300 rounded-2xl text-gray-700 placeholder-gray-500 focus:outline-none focus:border-primary" id="username" type="text" placeholder="อีเมล / หมายเลขโทรศัพท์"/>
+                  <span className="absolute left-4 text-gray-400 text-xl">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                      <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
+                    </svg>
+                  </span>
+                  <input className="w-full pl-12 pr-4 py-3 border-1 border-black rounded-2xl text-gray-700 placeholder-gray-500 focus:outline-none focus:border-primary" id="username" type="text" placeholder="อีเมล / หมายเลขโทรศัพท์"/>
                 </div>
                 <div className="mb-8 relative flex items-center">
-                  <span className="absolute left-4 text-gray-400 text-xl">🔒</span>
-                  <input className="w-full pl-12 pr-4 py-3 border-2 border-gray-300 rounded-2xl text-gray-700 placeholder-gray-500 focus:outline-none focus:border-primary" id="password" type="password" placeholder="รหัสผ่าน"/>
+                  <span className="absolute left-4 text-gray-400 text-xl">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                      <path fillRule="evenodd" d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3c0-2.9-2.35-5.25-5.25-5.25Zm3.75 8.25v-3a3.75 3.75 0 1 0-7.5 0v3h7.5Z" clipRule="evenodd" />
+                    </svg>
+                  </span>
+                  <input className="w-full pl-12 pr-4 py-3 border-1 border-black rounded-2xl text-gray-700 placeholder-gray-500 focus:outline-none focus:border-primary" id="password" type="password" placeholder="รหัสผ่าน"/>
                 </div>
                 <div className='text-right mb-10'>
                   <a className="text-sm font-bold text-gray-700 hover:text-primary" href="#">
