06bd3efb4febbb785ceffef3ea85432b59bb04c5
unknown <melyssa1857@gmail.com>
2026-04-09 12:33:32 +0700

    update detail page

--- app/director/evaluate/detail/[id]/page.tsx
+++ app/director/evaluate/detail/[id]/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+import { useEffect, useState } from "react";
 import { complaints } from "../../table/data"
 import { notFound } from "next/navigation"
 import StatusBadge from "../../table/statusBadge"
 import StatusBadge2 from "../../table/StatusBadge2"
+import SummaryCard from "@/components/ui/Admin_director/SummaryCard"
+import type { ComponentType, SVGProps } from 'react'
 
 type Props = {
   params: Promise<{
@@ -19,21 +23,53 @@ export default async function DetailPage({ params }: Props) {
   }
 
   return (
-    <div className="min-h-screen bg-background">
-      <div className="w-full px-8 py-8 mx-auto">
-        <div className="flex flex-row">
-          <h1 className="text-3xl font-bold text-[#333847] mb-7 ml-10">
-            รายละเอียดคำร้อง
-          </h1>
-          <h1 className="text-3xl font-bold text-[#725C00] mb-7 ml-8">
-            {complaint.problems}
-          </h1>
-        </div>
-        <div className="ml-10">
-          <StatusBadge2 status={complaint.status}/>
-        </div>
-      </div>
+<div className="w-full px-10 flex items-center justify-between mx-8">
+  {/* ซ้าย */}
+  <div>
+    <div className="flex items-center gap-4 mt-2">
+      <h1 className="text-3xl font-bold text-[#333847]">
+        รายละเอียดคำร้อง
+      </h1>
+      <h1 className="text-3xl font-bold text-[#725C00]">
+        {complaint.problems}
+      </h1>
     </div>
+
+    <div className="mt-6">
+      <StatusBadge2 status={complaint.status} />
+    </div>
+  </div>
+
+  {/* ขวา */}
+  <div className="flex flex-col items-end gap-3 mr-14 mt-8">
+    <div className="flex gap-3">
+      <button className="inline-flex items-center justify-center rounded-xl border border-gray-300 px-4 py-2 text-gray-600 hover:bg-gray-100 transition">
+        ประวัติคำร้อง
+      </button>
+
+      <button className="inline-flex items-center justify-center rounded-xl border border-gray-300 px-4 py-2 text-gray-600 hover:bg-gray-100 transition">
+        ดูผลการดำเนินการ
+      </button>
+    </div>
+
+    <button className="inline-flex items-center justify-center rounded-xl mr-24 bg-yellow-400 px-6 py-2 text-black hover:bg-yellow-500 transition">
+      ประเมินการปฏิบัติงาน
+    </button>
+  </div>
+
+  <div className="flex flex-col">
+    {/*การ์ดส่วน1*/}
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6 mx-12 mb-6">
+      <SummaryCard
+        key={complaint.id}
+        title={complaint.title}
+        value={complaint.app}
+        subvalue={complaint.location}
+        color="#4287f5"
+        />
+    </div>
+  </div>
+</div>
     // <div className="p-8">
     //   <h1 className="text-2xl font-bold">{complaint.title}</h1>
     //   <p>เลขรับเรื่อง: {complaint.problems}</p>
--- app/director/evaluate/table/StatusBadge2.tsx
+++ app/director/evaluate/table/StatusBadge2.tsx
@@ -7,14 +7,14 @@ type Props = {
 export default function StatusBadge({ status }: Props) {
   return (
 <span
-  className={`px-3 py-2 rounded-full text-md font-bold border whitespace-nowrap absolute
+  className={`px-3 py-1.5 rounded-full text-sm font-bold border whitespace-nowrap absolute
     ${status === "ประเมินผลเสร็จสิ้น" && "bg-green-100 text-green-700 border-green-700/20"}
     ${status === "กำลังดำเนินการ" && "bg-yellow-100 text-yellow-800 border-yellow-700/20"}
     ${status === "ไม่รับเรื่อง" && "bg-red-100 text-red-700 border-red-700/20"}
   `}
 >
     <span className="flex items-center gap-2">
-        <FaCircle size={10}/>
+        <FaCircle size={11}/>
           {status}
     </span>
 </span>
