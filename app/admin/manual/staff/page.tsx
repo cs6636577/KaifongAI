dc4e932230f959964c26681bfa034fe254ef0092
cs6636135 <s6604062636135@email.kmutnb.ac.th>
2026-04-10 12:19:08 +0700

    คู่มือการใช้งาน

---
+++ app/admin/manual/staff/page.tsx
@@ -0,0 +1,106 @@
+"use client"
+import React from 'react'
+import EmojiButton from '@/components/ui/Director/EmojiButton'
+import EmojiButton2 from '@/components/ui/Director/EmojiButton2'
+import ComplaintSearchInput from '@/components/ui/Admin_director/ComplainSearchInput'
+import { RiDropdownList } from 'react-icons/ri'
+import ManualToolbar from '@/components/ui/Director/ManualToolbar'
+import ComplaintPagination from '@/components/ui/Admin_director/PageNavigation'
+import FileCard from '@/components/ui/Director/fileCard'
+import { FaPlus } from 'react-icons/fa6'
+
+type FileItem = {
+  title: string
+  description: string
+  date: string
+  datasize: string
+  filetype?: string
+  viewcount: string
+  image?: string
+}
+
+const data: FileItem[] = [
+  {
+    title: "คู่มือการรับเรื่องร้องเรียนเบื้องต้น",
+    description: "อธิบายขึ้นตอนการรับเรื่อง การตรวจสอบข้อมูลพื้นฐาน และการคัดกรองประเภท...",
+    date: "12 Oct 2023",
+    datasize: "2.4 MB",
+    filetype: "PDF",
+    viewcount: "1240",
+  },
+  {
+    title: "คู่มือการอัพเดตสถานะเรื่องร้อง...",
+    description: "คู่มือสำหรับเจ้าหน้าที่ในการเปลี่ยนแปลงสถานะ แจ้งความคืบหน้า และปิดงานอย่า...",
+    date: "14 Oct 2023",
+    datasize: "1.8 MB",
+    filetype: "PDF",
+    viewcount: "890",
+  },
+  {
+    title: "Infographic ขั้นตอนการทำงาน",
+    description: "สรุปขั้นตอนการทำงานทั้งหมดแบบเห็นภาพเดียวจบ เข้าใจง่าย เหมาะสำหรับบอร์ด...",
+    date: "1920x1080 px",
+    datasize: "4.5 MB",
+    filetype: "IMAGE",
+    viewcount: "2,105",
+  }
+]
+
+
+const StaffManualPage = () => {
+    const [currentPage, setCurrentPage] = React.useState(1)
+
+  return (
+    <div className="min-h-screen bg-background flex  justify-center">
+        <div className="max-w-7xl mx-3 px-6 sm:px-6 lg:px-8 py-8 w-full mt-5">
+            <div className="flex items-start justify-between">
+            <div>
+                <h1 className="text-3xl font-bold text-secondary mb-7">คู่มือการใช้งาน</h1>
+                <p className="text-lg text-muted-foreground -mt-4">
+                เอกสารสำหรับผู้ใช้งานระบบ KaiFongAI
+                </p>
+            </div>
+
+            <button className="bg-accent px-4 py-3 rounded-xl font-bold shadow-md cursor-pointer hover:bg-yellow-500 transition flex items-center gap-2">
+                <FaPlus />
+                <span className="text-black text-md">อัพโหลดคู่มือใหม่</span>
+            </button>
+        </div>
+
+            {/* <button className='bg-accent p-4 rounded-xl font-bold shadow-md cursor-pointer hover:bg-yellow-500 transition flex flex-row'>
+                <span>
+                    <FaPlus />
+                </span>
+                <span className=" ml-1 text-black text-md">
+                    อัพโหลดคู่มือใหม่
+                </span>
+            </button> */}
+
+            <div className="flex gap-5 mt-8">
+                <EmojiButton/>
+                <EmojiButton2/>
+            </div>
+
+            <div className='mt-8'>
+                <ManualToolbar/>
+            </div>
+
+            <div className='flex flex-row gap-10 mt-10'>
+                {data.map((item, index) => (
+                    <FileCard key={index} item={item} />
+                ))}
+            </div>
+
+            <div className='mb-10 mt-40'>
+                <ComplaintPagination
+                    currentPage={1}
+                    totalPages={3}
+                    onPageChange={setCurrentPage}
+                />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default StaffManualPage
